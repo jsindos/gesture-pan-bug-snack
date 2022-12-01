c0b9aff007a8342b2e904b546bdc7e38cd18edb5
Joe Tsindos <jtsindos@gmail.com>
2022-12-01 12:48:54 +1100

    init

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Animated, Dimensions } from 'react-native'
+import { Gesture, GestureDetector, GestureHandlerRootView } from 'react-native-gesture-handler'
+import { useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
 
-export default function App() {
+const { width } = Dimensions.get('window')
+
+export default ({ navigation }) => {
+  const translate = new Animated.ValueXY({ x: 0, y: 0 })
+  const translateX = translate.x.interpolate({
+    inputRange: [-(width / 2), width / 2],
+    outputRange: [-(width / 2), width / 2],
+    extrapolateLeft: 'clamp',
+    extrapolateRight: 'clamp'
+  })
+
+  const [currentX, setCurrentX] = useState(0)
+
+  const panGesture = Gesture.Pan()
+    .onUpdate((e) => {
+      translate.setValue({ x: e.translationX + currentX, y: 0 })
+    })
+    .onEnd((e) => {
+      setCurrentX(e.translationX + currentX)
+    })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style='auto' />
+      <GestureDetector gesture={panGesture}>
+        <Animated.View
+          height={width / 1.5}
+          width={width}
+          style={{ position: 'absolute', zIndex: 1, transform: [{ translateX }], backgroundColor: '#000' }}
+        />
+      </GestureDetector>
+    </GestureHandlerRootView>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
